97e4459efb72734e48691777952947c5c9f7d74e
AminaGrd <amina.giraud@gmail.com>
2022-09-05 11:29:34 +0200

    first commit

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -1,9 +1,27 @@
-<!-- <nav>
+<nav>
     <div class="nav__logo"><img onclick="location.href='<?php bloginfo('wpurl'); ?>';" src="<?php echo get_site_icon_url(); ?>" alt="icone du site"></div>
-    <?php wp_nav_menu( array( 'menu' => 'navbar' ) ); ?>
+    <!-- <?php wp_nav_menu( array( 'menu' => 'navbar' ) ); ?> -->
     <div class="nav__burger" id="burger">
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+            <a href="#">Accueil</a>
+        </div>
+        <div>
+            <a href="#">Notre Entreprise</a>
+        </div>
+        <div>
+            <a href="#">Nos Expertises</a>
+        </div>
+        <div>
+            <a href="#">Nos Marchés</a>
+        </div>
+        <div>
+            <a href="#">Nos Produits</a>
+        </div>
+        <div>
+            <a href="#">Contact</a>
+        </div>
     </div>
-</nav> -->
+    <div>
+        <a href="#">04 74 42 32 18</a>
+    </div>
+</nav>
