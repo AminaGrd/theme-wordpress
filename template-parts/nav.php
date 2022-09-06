f3e236bbdc5d5a6127a6b5cfe815bbd3a4ce953c
AminaGrd <amina.giraud@gmail.com>
2022-09-06 17:20:01 +0200

    nav

--- template-parts/nav.php
+++ template-parts/nav.php
@@ -5,6 +5,7 @@
     <?php wp_nav_menu( array( 'menu' => 'navbar' ) ); ?>
     <div class="nav__contact">
         <a href="tel:0474423218">04 74 42 32 18</a>
+        <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/ico/logo-tel.png">
     </div>
     <div class="nav__burger" id="burger">
         <div></div>
