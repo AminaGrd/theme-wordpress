a02aadb482ddf70f8698261bd733ac84f207064e
AminaGrd <amina.giraud@gmail.com>
2022-09-07 13:56:38 +0200

    menu burger animation cross

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,14 +1,17 @@
 const burger = document.getElementById('burger');
 const inMenu = document.querySelector("nav ul.menu");
 const navbarlanding = document.getElementsByClassName('menu-navbar-container')[0]; // cible nav wp list
+const menuburger = document.getElementsByClassName('nav__burger')[0];
 let i = 0;
 
 burger.addEventListener('click', function() {
     toogleMenu();
+    crossMenu();
 })
 inMenu.addEventListener('click', function() { // si on clique sur le menu
     if (i != 0) {
         toogleMenu();
+        crossMenu();
     }
 })
 
@@ -17,6 +20,13 @@ function toogleMenu() {
     navbarlanding.classList.toggle('menu-navbar-container--show');
     if (i >= 2) {
         navbarlanding.classList.toggle('menu-navbar-container--unshow');
+    }
+}
+function crossMenu() {
+    i++;
+    menuburger.classList.toggle('nav__burger--cross');
+    if (i >= 3) {
+        menuburger.classList.toggle('nav__burger--burger');
     }
 }
 
